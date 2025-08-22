34ac868fa0df3133963d29883322a71be9a27f8f
Dev <devkdarji@gmail.com>
2025-08-23 00:11:56 +0530

    Create vol_of_Cuboid_Class_Method.cpp

---
+++ vol_of_Cuboid_Class_Method.cpp
@@ -0,0 +1,30 @@
+//Dev Darji
+//24070123033
+//Batch: A2
+
+#include <iostream>
+using namespace std;
+class cuboid {
+    public:
+    int height = 3;
+    int width = 2;
+    int length = 4;
+
+
+    int volume() {
+    int v;
+    v = height * width * length;
+    
+    return v;
+    }
+};
+
+int main(){
+    cuboid cuboid1;
+    int vol = cuboid1.volume();
+    cout<<"Volume: "<<vol<<endl;
+}
+
+# OUTPUT:
+
+Volume: 24
